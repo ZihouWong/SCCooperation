f7ed86222a26bb3758d5ad1282c97dbe50ee7dc8
ZihouWong <wongzihou1997@gmail.com>
2018-06-10 04:09:49 +0800

    添加action基本内容

--- src/org/sccooperation/gongxingketang/action/ShowMoreCourseAction.java
+++ src/org/sccooperation/gongxingketang/action/ShowMoreCourseAction.java
@@ -1,5 +1,48 @@
 package org.sccooperation.gongxingketang.action;
 
+import java.util.Map;
+
 public class ShowMoreCourseAction {
 
+	// 信息：
+	private Map request;
+	private String pageNo;
+	private String content;	// 搜索内容
+	private String button; 
+	
+	public Map getRequest() {
+		return request;
+	}
+
+	public void setRequest(Map request) {
+		this.request = request;
+	}
+
+	public String getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(String pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getButton() {
+		return button;
+	}
+
+	public void setButton(String button) {
+		this.button = button;
+	}
+
+	public String execute() {
+		return "success";
+	}
 }
